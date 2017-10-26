62e757b69bfcd01794d64ed6cd94bf8c27171548
Cat Janowitz and Lew Davidson <cat@>
2017-10-26 09:49:50 -0700

    add form and confirm that input is returning a number yay

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,54 @@
-var I = 1;
-var V = 5;
-var X = 10;
-var L = 50;
-var C = 100;
-var D = 500;
-var M = 1000;
+// var I = 1;
+// var V = 5;
+// var X = 10;
+// var L = 50;
+// var C = 100;
+// var D = 500;
+// var M = 1000;
+//
+// var roman = [I, V, X, L, C, D, M];
+//
+//
+//
+// var converter = function(parameter) {
+//   for () {
+//     for () {
+//       if () {
+//
+//       } else {
+//
+//       }
+//     }
+//   }
+// }
+
+
+$(document).ready(function() {
+  $("form#roman").submit(function(event) {
+    event.preventDefault();
+    var inputNum = parseInt($("input#submitted").val());
+      console.log(inputNum);
+    // var check = function(value) {
+    //   return Number.isNaN(Number(inputNum));
+    //
+    // }
+  });
+
+});
+
+
+
+
+
+
+
+//   for (var iInput = 0; iInput <= input.charAt(0); iInput ++) {
+//     for(var iRoman = 0; iRoman <= roman.length; iRoman ++)
+//       if (input[iInput] === roman[iRoman]) {
+//       return(I);
+//     }  else {
+//       return(V);
+//     }
+//
+//   }
+// };
